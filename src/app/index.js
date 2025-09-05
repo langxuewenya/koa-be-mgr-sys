6acfca1678d8ff7359fb24eadb8a135be67da830
langxuewenya <langxuewenya@163.com>
2025-09-05 23:45:31 +0800

    cors
    配置跨域

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,16 @@
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const registerRouteers = require("../router");
+const cors = require("@koa/cors");
 
 // 创建app
 const app = new Koa();
 
 // 对app使用中间件
 app.use(bodyParser());
+
+// 配置跨域，允许所有来源
+app.use(cors());
 
 // 动态注册路由对象
 registerRouteers(app);
